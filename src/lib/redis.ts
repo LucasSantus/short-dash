254c863e7f0d80a7609b8882eb3b24d20829585b
Lucas Santos <leos.developer@gmail.com>
2024-11-26 18:38:24 -0300

    feat: remove redis

--- src/lib/redis.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "@/env";
-import Redis from "ioredis";
-
-export const redis = new Redis({
-  host: env.REDIS_HOST,
-  port: Number(env.REDIS_PORT),
-  password: env.REDIS_PASSWORD || undefined,
-});
