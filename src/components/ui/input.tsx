ce938ee97973c54c551b610f1d887d647b62d910
Lucas Santos <leos.developer@gmail.com>
2024-11-27 20:42:11 -0300

    feat: update shadcn components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,14 +11,15 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, isLoading, startComponent, endComponent, ...props }, ref) => {
-    const classes = "absolute bottom-0 top-0 z-10 flex h-full w-10 items-center justify-center";
+    const classes =
+      "absolute bottom-0 top-0 z-10 flex h-full w-10 items-center justify-center [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0";
 
     return (
       <div className="relative">
         <input
           type={type}
           className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             className,
             { "pl-12": !!startComponent },
             { "pr-12": !!endComponent }
