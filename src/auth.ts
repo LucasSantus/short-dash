940554aaa1f2163a618e9ca1a815ee11af9371f0
Lucas Santos <leos.developer@gmail.com>
2024-12-18 23:37:06 -0300

    feat: remove account of token jwt

--- src/auth.ts
+++ src/auth.ts
@@ -1,75 +1,13 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { compare } from "bcryptjs";
-import NextAuth, { Account } from "next-auth";
+import NextAuth from "next-auth";
 import type { Adapter } from "next-auth/adapters";
+import { authConfig } from "./auth.config";
 import { env } from "./env";
-import { CredentialsAccountNotFoundError } from "./errors/auth/credentials-account-not-found";
-import { CredentialsPasswordNotFoundError } from "./errors/auth/credentials-password-not-found-error";
-import { CredentialsPasswordNotIsValidError } from "./errors/auth/credentials-password-not-is-valid-error";
-import { CredentialsUserDeactivateError } from "./errors/auth/credentials-user-deleted";
-import { CredentialsUserNotFoundError } from "./errors/auth/credentials-user-not-found";
 import { prismaClient } from "./lib/prisma";
-import { signInFormSchema } from "./validation/auth/sign-in";
-
-import Credentials from "next-auth/providers/credentials";
-import Google from "next-auth/providers/google";
-import Resend from "next-auth/providers/resend";
 
 export const { auth, handlers, signIn, signOut, unstable_update } = NextAuth({
   session: { strategy: "jwt" },
   adapter: PrismaAdapter(prismaClient) as Adapter,
-  providers: [
-    Google({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
-    }),
-    Resend({
-      from: env.RESEND_TO_EMAIL,
-    }),
-    Credentials({
-      name: "Credentials",
-      credentials: {
-        email: { label: "E-mail", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const { email, password } = await signInFormSchema.parseAsync(credentials);
-
-        const user = await prismaClient.user.findUnique({
-          where: {
-            email,
-          },
-        });
-
-        if (!user) throw new CredentialsUserNotFoundError();
-
-        if (user.deletedAt) throw new CredentialsUserDeactivateError();
-
-        const account = await prismaClient.account.findFirst({
-          where: {
-            userId: user.id,
-            provider: "credentials",
-          },
-        });
-
-        if (!account) throw new CredentialsAccountNotFoundError();
-
-        if (!user.password)
-          throw new CredentialsPasswordNotFoundError("Ocorreu um problema ao tentar recuperar a conta do usuário!");
-
-        const isPasswordValid = await compare(password, user.password);
-
-        if (!isPasswordValid) throw new CredentialsPasswordNotIsValidError();
-
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-        };
-      },
-    }),
-  ],
   secret: env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/sign-in",
@@ -77,7 +15,7 @@ export const { auth, handlers, signIn, signOut, unstable_update } = NextAuth({
     error: "/failed",
   },
   callbacks: {
-    async jwt({ token, user, trigger, session, account }) {
+    async jwt({ token, user, trigger, session }) {
       if (trigger === "update" && !!session) {
         /**
          * Todos os campos que seram alterados devem ser passados aqui.
@@ -86,10 +24,6 @@ export const { auth, handlers, signIn, signOut, unstable_update } = NextAuth({
          *  token.email = session?.email;
          */
         token.name = session?.name;
-      }
-
-      if (account) {
-        token.account = account;
       }
 
       if (user)
@@ -103,15 +37,13 @@ export const { auth, handlers, signIn, signOut, unstable_update } = NextAuth({
 
     async session({ session, user, trigger, token, newSession }) {
       if (token) {
-        const { user, account } = token as {
+        const { user } = token as {
           user: {
             id: string;
           };
-          account: Account;
         };
 
         session.user = { ...session.user, id: user.id };
-        session.account = account || null;
       } else if (trigger === "update") {
         /**
          * Todos os campos que seram alterados devem ser passados aqui.
@@ -130,4 +62,5 @@ export const { auth, handlers, signIn, signOut, unstable_update } = NextAuth({
       return session;
     },
   },
+  ...authConfig,
 });
